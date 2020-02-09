908100b876841a63f66e0df35e7e14d59be58d39
talk2sohail <talk2sohail100@gmail.com>
2020-02-09 14:53:55 +0530

    npm audit fix

--- config/index.js
+++ config/index.js
@@ -1,6 +1,26 @@
 const key = {
-  PORT: process.argv.PORT || 8080,
-  mongoURI: "mongodb://localhost/api2"
+	PORT: process.argv.PORT || 8080,
+	mongoURI: "mongodb://localhost/api2"
+};
+const development = {
+	development: {
+		sitename: "API for order[development]",
+		data: {
+			order: path.join(__dirname + "../data/order.json")
+		}
+	}
+};
+const production = {
+	production: {
+		sitename: "API for order",
+		data: {
+			order: path.join(__dirname + "../data/order.json")
+		}
+	}
 };
 
-module.exports = key;
+module.exports = {
+	key,
+	development,
+	production
+};
